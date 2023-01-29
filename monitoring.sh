65ee17a2ddb1ae1fb1f20394651c57e55af623c3
Rodolfo Freitas <rodolfoc@gmail.com>
2023-01-29 15:25:38 +0000

    Correção
    Correção com relação as % de memoria e disco usadosrelacao

--- monitoring.sh
+++ monitoring.sh
@@ -7,8 +7,8 @@ fram=$(free --mega | awk '$1 == "Mem.:" {print $2}')
 uram=$(free --mega | awk '$1 == "Mem.:" {print $3}')
 pram=$(free --mega | awk '$1 == "Mem.:" {printf("%.2f"), $3/$2*100}')
 fdisk=$(df -Bg | grep '^/dev/' | grep -v '/boot$' | awk '{ft += $2} END {print ft}')
-udisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} END {print ut}')
-pdisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} {ft+= $2} END {printf("%d"), ut/ft*100}')
+udisk=$(df -Bg | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} END {print ut}')
+pdisk=$(df -Bg | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} {ft+= $2} END {printf("%d"), ut/ft*100}')
 cpul=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
 lrb=$(who -b | awk '$1 == "arranque" {print $4 " " $5}')
 lvmt=$(lsblk | grep "lvm" | wc -l)
@@ -23,8 +23,8 @@ wall "	#Operating System: $os
 	#Kernel: $ker
 	#CPU physical: $pcpu
 	#CPU virtual: $vcpu
-	#Memory Usage: $uram/${fram}MB ($pram%)
-	#Disk Usage: $udisk/${fdisk}Gb ($pdisk%)
+	#Memory Usage: ${uram}MB/${fram}MB ($pram%)
+	#Disk Usage: ${udisk}Gb/${fdisk}Gb ($pdisk%)
 	#CPU load: $cpul
 	#Last reboot: $lrb
 	#LVM use: $lvmu
